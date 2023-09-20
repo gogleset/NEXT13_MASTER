9a70d311244aba6b48f0e7764727a28c9565e56d
gogleset <gogleset@github.com>
2023-09-20 15:59:58 +0900

    course6

--- my-app/app/api/post/route.ts
+++ my-app/app/api/post/route.ts
@@ -1,24 +1,27 @@
 import { NextResponse, NextRequest } from "next/server"
+import { headers } from "next/headers"
 
 export async function GET(request: NextRequest) {
-    const url = "https://jsonplaceholder.typicode.com"
-    const res = await fetch(`${url}/posts`,{next:{revalidate:60}})
-    const result = await res.json()
-  
-    return NextResponse.json({result});
-}
- 
-export async function POST(request: NextRequest) {
+  const httpHeaders = headers()
+  console.info("httpHeaders: ", httpHeaders)
+  console.info("request.headers: ", request.headers)
+  const url = "https://jsonplaceholder.typicode.com"
+  const res = await fetch(`${url}/posts`, { next: { revalidate: 60 } })
+  const result = await res.json()
 
-    return NextResponse.json({ok: true, status: 200});
+  return NextResponse.json({ result })
 }
- 
+
+export async function POST(request: NextRequest) {
+  return NextResponse.json({ ok: true, status: 200 })
+}
+
 export async function PUT(request: Request) {
-    console.log(request.method)
-    return NextResponse.json({ok: true , status: 200 });
+  console.log(request.method)
+  return NextResponse.json({ ok: true, status: 200 })
 }
- 
+
 export async function DELETE(request: Request) {
-    console.log(request.method)
-    return NextResponse.json({ok: true , status: 200 });
+  console.log(request.method)
+  return NextResponse.json({ ok: true, status: 200 })
 }
---
+++ my-app/middleware.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log("middleware")
+
+  // proxy
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    console.log(request.url, "middleware, /dashboard")
+    // Router로 넘기기 가능(중간다리 middleware이기 때문)
+    const response = NextResponse.next()
+    response.cookies.set({
+      name: "foo",
+      value: "true",
+      path: "/",
+    })
+
+    return response
+  }
+
+  if (request.nextUrl.pathname.startsWith("/login")) {
+    console.log(request.url, "middleware, /login")
+    let verify = true
+    if (verify) {
+      return NextResponse.redirect(new URL("/", request.url))
+    }
+  }
+
+  if (request.nextUrl.pathname.startsWith("/api/post")) {
+    console.log(request.url, "middleware, /api/post")
+    const newHeader = new Headers(request.headers)
+    newHeader.set("some-thing-header", "something from header")
+    // Router로 넘기기 가능(중간다리 middleware이기 때문)
+    const response = NextResponse.next({
+      request: {
+        headers: newHeader,
+      },
+    })
+    return response
+  }
+}
+
+export const config = {
+  matcher: ["/dashboard", "/login/:path*", "/api/post/:path*"],
+}
